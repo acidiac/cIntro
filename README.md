831d5e853095a14d5b4e0d74a723d97483ac3d14
Amit Chanchal <amit.chanchal@hotmail.com>
2016-10-27 01:42:47 +0530

    section1chap3

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # cIntro
 
 Section 1 --- Overview and Fundamentals
+        unit1 -- Basics and syntax and structure os a C program
+        unit2 -- Variables and Datatypes
+        unit3 -- Strings and operations on strings
 
 
 
---
+++ section1/2_3.c
@@ -0,0 +1,10 @@
+/*
+    Introduction to C programming
+    Section1 (Overview and Fundamentals)
+    2_3 Booleans in C  
+    - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    Author: Amit Chanchal
+    Date: 25 October, 2016
+    amit.chanchal@hotmail.com
+    - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
