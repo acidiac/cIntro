c16371c780279399c2a3af39b2552982238dc8bb
Amit Chanchal <amit.chanchal@hotmail.com>
2016-10-26 12:41:50 +0530

    sec1cha2

--- section1/2_2.c
+++ section1/2_2.c
@@ -22,6 +22,20 @@
 */    
 
 int main ( void ){
+    int age;    // Declaring a variable with label "age" (which stores age of a person) and int is the datatype
+    age = 34;   // Initializing the variable with value 34.
+    printf("My age is %d\n",age);
+    /*
+        printf essentially used for formatted printing as oppose to functions [ puts() or putchar()] which do not format the output.__arg
+        The formatting can be done depending on the datatype of the variable. So here the variable "age" is a decimal value and for that we use 
+        %d, which is a placeholder for the value of decimal datatype. The variables and values declared after the statement in quotes replace placeholders 
+        in sequence.
+    */
+
+    // Read more about datatypes << link >>
+    // Read more about the print formatting in C  << link >>
+}
+
+void maxValues() {
     
-  
 }
