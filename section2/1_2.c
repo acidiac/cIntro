97b0705b6b404de362621bb97746fcf33a8d957b
Amit Chanchal <amit.chanchal@hotmail.com>
2016-10-28 01:00:10 +0530

    Section2

---
+++ section2/1_2.c
@@ -0,0 +1,11 @@
+/*
+    Introduction to C programming
+    Section2 (Control and Loops)
+    1_2 If Else and Nested If
+    - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    Author: Amit Chanchal
+    Date: 25 October, 2016
+    amit.chanchal@hotmail.com
+    - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+*/
---
+++ section2/2_1.c
@@ -0,0 +1,11 @@
+/*
+    Introduction to C programming
+    Section2 (Control and Loops)
+    2_1 For Loops 
+    - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    Author: Amit Chanchal
+    Date: 25 October, 2016
+    amit.chanchal@hotmail.com
+    - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+*/
