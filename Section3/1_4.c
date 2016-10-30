be4b3226bbb45f7d34b83a0362722b92a4c7aab9
Amit Chanchal <amit.chanchal@hotmail.com>
2016-10-30 14:40:58 +0530

    sec3

--- Section3/1_4.c
+++ Section3/1_4.c
@@ -12,14 +12,23 @@
 #include <stdlib.h>
 
 int main (void) {
+    char myChar = 'a';
+    char *myPtr;
     int arr1[ ] = {2,5,54};
-    char arr2[ ] = {"a", "b"};
+    char arr2[ ] = {'a', 'b'};
     float arr3[ ] = {23.54, 1.12};
     int *intPtr = arr1;
     char *charPtr = arr2;
     float *floatPtr = arr3;
 
-    printf("Is size of int and int Pointer same? [%d]\n", (sizeof(intPtr)==sizeof(int)));
+   //printf("Is size of int and int Pointer same? [%d]\n", (sizeof(intPtr)==sizeof(int)));
+    // Surprisingly we are getting 0 which means theyare not equal!!
+
+    printf("Size of the pointer variable %lu \t", sizeof(myPtr));
+    printf("Size of integer variable %lu\n", sizeof(char));
+    
+    //puts(sizeof(intPtr));    
+    //puts(sizeof(int));
     return 0;
 
 }
