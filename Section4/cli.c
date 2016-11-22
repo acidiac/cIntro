0aa8fa6e364c970829609183f1e0ff9dedf40c2a
Amit Chanchal <amit.chanchal@hotmail.com>
2016-11-23 01:12:54 +0530

    cli

--- Section4/cli.c
+++ Section4/cli.c
@@ -1,34 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main ( int argc, char *argv[] )
-{
-    if ( argc != 2 ) /* argc should be 2 for correct execution */
-    {
-        /* We print argv[0] assuming it is the program name */
-        printf( "usage: %s filename", argv[0] );
-    }
-    else 
-    {
-        // We assume argv[1] is a filename to open
-        FILE *file = fopen( argv[1], "r" );
+int main( int argc, char* argv[]){
+    if (argc == 1){
+        printf("Please provide command line arguments\nFor help use h\n ");
+        return 0;
+    } else if (argc < 3 || argc > 3){
+        printf("Too few arguments\nFor help use h\n ");
+        return 0;
+    } 
 
-        /* fopen returns 0, the NULL pointer, on failure */
-        if ( file == 0 )
-        {
-            printf( "Could not open file\n" );
-        }
-        else 
-        {
-            int x;
-            /* read one character at a time from file, stopping at EOF, which
-               indicates the end of the file.  Note that the idiom of "assign
-               to a variable, check the value" used below works because
-               the assignment statement evaluates to the value assigned. */
-            while  ( ( x = fgetc( file ) ) != EOF )
-            {
-                printf( "%c", x );
-            }
-            fclose( file );
-        }
+    if (argv[1]== 'h'){
+        printf(" pass two number followed by the operation, below are the list of operations\n");
+        printf("add for addition\n ");
+        printf("sub for Subtraction\n");
+        printf("mul for Multiplication\n");
+        printf("------------------------------------------------------------\n");
+    } else {
+           int x = (int)argv[1];
+        int y = (int)argv[2];
+        printf("sum of the numbers: %d", x+y);
     }
+
+    return 0;
 }
