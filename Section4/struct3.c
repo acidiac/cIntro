ad08bc13c8ff34fefc593b6343296a0f9fbfb26c
Amit Chanchal <amit.chanchal@hotmail.com>
2016-11-22 23:43:33 +0530

    structure- pass by reference

--- Section4/struct3.c
+++ Section4/struct3.c
@@ -1,2 +1,31 @@
 /* Structure passing by reference */
+#include<stdio.h>
+
+typedef struct {
+    int emp_id;
+    double salary;
+} employee;
+
+void annualRaise (employee* emp);
+void printEmp(employee* emp);
+
+int main(void){
+    employee noname = {1001, 45000};
+    employee* employeePtr;
+    employeePtr = &noname;
+    // Giving employee a hike 
+    annualRaise(employeePtr);
+    
+    return 0;
+}
+
+void annualRaise (employee* emp) {
+    emp->salary += emp->salary * 0.1;
+    printf("Salary of employee ID: %d hiked by 10%", emp->emp_id);
+}
+
+void printEmp(employee* emp){
+    printf("Employee Id: %d \n", emp->emp_id); 
+    printf("Employee Salary:  %.2f \n", emp->salary);
+}
 
