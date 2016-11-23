c238354d25083f1144dd9a644759f0d25f332135
Amit Chanchal <amit.chanchal@hotmail.com>
2016-11-23 11:33:53 +0530

    cli prime

--- Section4/cli_prime.c
+++ Section4/cli_prime.c
@@ -1,14 +1,28 @@
 /* Program to demonstrate commmand line arguments as inputs */
 #include<stdio.h>
+// Function prototype declaration
 int isPrime(int n);
 
+/*
+    we provide main with two parameters argc and argv[ ],
+    -- argc of int type keep total count of arguments provided,
+    -- argv[ ] of char* type stores all the arguments 
+*/
 int main (int argc, char* argv[]){
   int i;
+  // First argument argv[0] is the name of the file
+  printf("Evaluating file %s ---------------  ------  ------ \n", argv[0]);
   for (i = 1; i < argc; i++ ){
       int num;
+      /*
+        arguments in argv are of char type and we are using 
+        sscanf to read the input and store it as a int type. 
+        Syntax: sscanf(inputVar, <format string>, pointerVar);
+      */
       sscanf(argv[i],"%d", &num ) ;
       isPrime(num);
   }
+  printf("----------Finished Execution ---------------  ------  ------ \n");
   return 0;
 }
 
