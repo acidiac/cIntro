7e50d8a15377f8021c08128382189eaa4fd4b800
Amit Chanchal <amit.chanchal@hotmail.com>
2016-12-03 01:48:57 +0530

    linked list

---
+++ Section5/linkedlist.c
@@ -0,0 +1,20 @@
+/*
+    Singly linked list implementation
+    Author: Amit Chanchal
+    Date: 21-Nov-2016
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// definition of the structure with data to hold our value and link to store the pointer to next node
+struct node_proto{
+    int data;
+    struct* node link;
+} ;
+
+int main (int argc, char* argv[]){
+    
+}
+
+
